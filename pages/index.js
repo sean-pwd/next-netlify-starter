fdd6c311e48c3a3a45e3bd7a748124f4caacd564
sean-pwd <sean@powerfulwebdesign.com.au>
2021-08-08 20:19:58 +1000

    fix: add ccu

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <h2>Castle Craft Upholstery</h2>
+        <a href="https://app.netlify.com/sites/castlecraft/deploys">
+          <img src="https://api.netlify.com/api/v1/badges/8d1fc77c-309b-44cd-9157-517f1dd73110/deploy-status"></img>
+        </a>
       </main>
 
       <Footer />
